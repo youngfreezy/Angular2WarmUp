2c988782309268856500b9f2fc6275fe70502373
Fareez Ahmed <fareez.ahmed@onedatascan.com>
2017-02-07 13:58:44 -0500

    finished shell

---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+    // instantiate posts to an empty array
+  posts: any = [];
+  constructor(private postsService: PostsService) { }
+
+  ngOnInit() {
+    this.postsService.getAllPosts().subscribe( posts => {
+      this.posts = posts;
+    })
+  }
+}
